ef5c423b1f588d980f1f784d6abdd6ef5bfba83a
Developer-Shuvo <im.computer.engineeer@gmail.com>
2025-05-09 16:20:34 +0600

    Little function practice

---
+++ practice.js
@@ -0,0 +1,25 @@
+const firstName = "Md. Nazmul";
+const middleName = "Hasan";
+const lastName = "Shuvo";
+
+// const fullName = firstName + middleName + lastName;
+// console.log(fullName);
+
+const fullName = `${firstName} + ${middleName} + ${lastName}`;
+// console.log(fullName);
+
+const part1 = `amar sunar bangla
+ami tumay valobashi
+chirodin tumar akash
+tumar batash`;
+
+console.log(part1);
+
+// Function practice
+function add(a, b) {
+  const output = a + b;
+  return output;
+}
+
+const sum = add(5, 7);
+console.log("Your Mathemetical calculation result is: =", sum);
