b6b8f0e438f3d79076fff76a6b35522d81e7c463
Developer-Shuvo <im.computer.engineeer@gmail.com>
2025-05-09 19:37:40 +0600

    Normal, Expression & Arrow function practice

--- practice.js
+++ practice.js
@@ -15,11 +15,27 @@ tumar batash`;
 
 console.log(part1);
 
-// Function practice
+// different ways to use function
+
+//Normal Function ()
 function add(a, b) {
   const output = a + b;
   return output;
 }
 
 const sum = add(5, 7);
-console.log("Your Mathemetical calculation result is: =", sum);
+console.log("Your Normal function result is: =", sum);
+
+//function expression
+
+const add2 = function (a, b) {
+  return a + b;
+};
+
+const sum2 = add2(10, 5);
+console.log("your Function expression result is: =", sum2);
+
+// Arrow function
+const add3 = (a, b) => a + b;
+const sum3 = add3(30, 30);
+console.log("Your arrow function result is: =", sum3);
