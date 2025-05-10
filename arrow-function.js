f1025e25c69a68f33e1e10e956d1caaa9d49d73d
SHUVO <im.computer.engineeer@gmail.com>
2025-05-10 17:55:54 +0600

    Arrow function using practice

---
+++ arrow-function.js
@@ -0,0 +1,21 @@
+// Question: Write an arrow function that will take 3 parameter and multiply and show output result.
+// Arrow function
+
+const multiply = (a, b, c ) => a * b * c;
+const  result= multiply(10, 5, 9);
+console.log ('Take 3 parameter and multiply them, Final result is : =', result) ;
+
+
+
+
+
+
+
+
+// Write a sentence in 3 lines and print it 3 lines
+
+const description =`I am a student
+My goal is to become a successful web developer
+Allah with me and i will achieve the goal one day isnhaa-allah.`
+
+console.log (description);
