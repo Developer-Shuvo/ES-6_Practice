75caa53d70da1885ea738e6268a6fe9a3c7b8dd3
SHUVO <im.computer.engineeer@gmail.com>
2025-05-12 22:56:00 +0600

    function learning

---
+++ function.js
@@ -0,0 +1,145 @@
+// function gan(paro) {
+//     console.log(paro);
+// }
+
+// gan('Parina');
+
+// function square(number) {
+//     console.log(number);
+// }
+
+// square(5);
+
+
+// **********************************************************
+// function gan(paro) {
+//     console.log(paro);
+// }
+
+// gan('Pari kintu soob to abeg er gaan')
+
+
+
+// *************************Borgo ber kora*********************************
+
+// function square(number) {
+//     console.log('The given number:', number);
+//     let borgo = number * number;
+//     console.log('Answer of this number is :', borgo);
+// }
+
+// square(10);
+
+
+
+// // *************************Borgo ber kora*********************************
+
+// function square(number) {
+//     console.log('The question number is :', number)
+//     let borgo = number * number;
+//     console.log('The answer number is :', borgo)
+// }
+
+// square(5);
+// square(35);
+// square(100);
+// square(3000);
+
+
+
+
+// // **********************************************************
+
+
+// function friendship(name1, name2) {
+//     console.log('They are good friends:', name1, 'and', name2);
+//     console.log('Friendship:', friendship);
+// }
+
+// friendship('Shameem' ['']
+//     'Saqib');let friendship = `[name1] & [name2]`;
+
+
+
+// // **********************My age and Father age difference************************************
+
+// function difference(num1, num2) {
+//     let diff = num1 - num2;
+//     console.log(num1, num2, 'difference is:', diff);
+// }
+
+// let fatherAge = 45;
+// let myAge = 22;
+
+// difference(fatherAge, myAge);
+
+
+// function difference(num1, num2) {
+//     let differenceValue = num1 - num2;
+//     console.log(num1, num2, 'The age difference is:', differenceValue);
+// }
+
+// const fatherAge = 45;
+// const myAge = 22;
+
+// difference(fatherAge, myAge);
+
+
+// function multiply(num1, num2, num3, num4) {
+//     let result = num1 * num2 * num3 * num4;
+//     console.log('The multiplication of the numbers is:', result);
+
+//     return result;
+// }
+
+// let number1 = 2;
+// let number2 = 2;
+// let number3 = 2;
+// let number4 = 2;
+
+// multiply(number1, number2, number3, number4);
+
+
+
+// ***************inch to feet*******************
+
+function inchToFeet(inch) {
+    let feet = inch / 12;
+    return feet;
+}
+
+let inches = 1200;
+let resultInFeet = inchToFeet(inches);
+console.log('The result in feet is:', resultInFeet);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function isEven(number) {
+
+//     if (number % 2 === 0) {
+//         console.log("this is an even number");
+//         return true;
+//     } else {
+//         console.log("this is an odd number");
+//         return false;
+//     }
+// }
+
+// console.log(isEven(10));
+// console.log(isEven(13));
