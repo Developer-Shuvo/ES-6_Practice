11d2d1f0252c5c2680c94e29f7cfde842cf1b784
SHUVO <im.computer.engineeer@gmail.com>
2025-05-12 22:55:20 +0600

    while-loop learning

---
+++ while-loop.js
@@ -0,0 +1,100 @@
+// let num = 20;
+// while (num <= 100) {
+//     console.log(num);
+//     num++;
+// }
+
+
+// **********Even Number find out 1 to 50*************
+
+// let number = 1;
+// while (number <= 50) {
+//     console.log(number);
+//     number++;
+
+//     if (number % 2 === 0) {
+//         console.log("Even Number is:", number)
+//     }
+// }
+
+
+
+// ** Odd number testing***
+
+// let number = 10;
+// while (number <= 100) {
+//     console.log(number);
+//     number++;
+
+
+//     if (number % 2 === 1) {
+//         console.lod("odd number is:", number)
+//     }
+
+
+
+// *****For loop******
+
+// for (let i = 5; i <= 20; i++) {
+//     console.log(i);
+// }
+
+
+// for (let num = 20; num <= 50; num++) {
+//     console.log(num);
+// }
+
+
+// for (let i = 1; i <= 20; i++) {
+//     console.log(i);
+// }
+
+
+// for (let i = 1; i <= 50; i = i + 2) {
+//     console.log('The odd number is;', i, 'among 1 to 50')
+// }
+
+// *******************************
+// for (let i = 5; i <= 30; i = i + 2) {
+//     console.log('The odd number is:', i, 'among 5 to 30')
+// }
+
+// for (let i = 33; i <= 100; i++) {
+//     if (i % 2 === 0) {
+//         console.log('The even number is:', i, 'among 33 to 100');
+//     }
+// }
+
+
+
+
+// let sum = 0; // Declare the variable 'sum'
+
+// for (let i = 12; i <= 60; i++) {
+//     sum = sum + i;
+//     console.log(i);
+// }
+
+// console.log('The sum of all numbers from 12 to 60 is:', sum);
+
+
+
+
+// let sum = 0;
+
+// for (let i = 1; i <= 40; i++) {
+//     sum = sum + i;
+
+// }
+// console.log('The sum of all numbers form 1 t0 40 is:', sum);
+
+
+
+// ***************The sum of all numbers from 1 to 100**************************
+// let sum = 0;
+
+// for (let i = 1; i <= 100; i++) {
+//     sum += i; // same as sum = sum + i
+// }
+
+// console.log('The sum of all numbers from 1 to 100 is:', sum);
