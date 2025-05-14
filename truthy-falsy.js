68bbffef6cfecf57015e4dc493605b37df13e966
SHUVO <im.computer.engineeer@gmail.com>
2025-05-14 23:27:08 +0600

    Truthy Falsy js

---
+++ truthy-falsy.js
@@ -0,0 +1,23 @@
+// True values
+// * false
+// * 5
+// * "hello"
+// * [1, 2, 3]
+// * { a: 1, b: 2 }
+
+
+
+// false values
+// * false
+// * 0
+// * ""
+// * null
+const a = 'This is a positive value';
+
+if (a){
+    console.log ('This is a truthy value');
+}
+
+else {
+    console.log ('This is a falsy value');
+} 
