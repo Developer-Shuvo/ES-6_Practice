4bbfdab97abfba826263a10eb32c5a577b5b7404
SHUVO <im.computer.engineeer@gmail.com>
2025-05-14 23:27:26 +0600

    Type js

---
+++ type.js
@@ -0,0 +1,15 @@
+// Primitive Data Types
+let a = 10;
+let b = a;
+b = 50;
+console.log (a,b);
+
+
+// ______________________________________________________________________________________
+// Reference Data Types
+let student = { name: "john", Passed : true};
+let teacher = student;
+teacher = { name: 'Kamal sir', experience: 10};
+
+
+console.log (student, teacher);
