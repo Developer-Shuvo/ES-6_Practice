9e9ab3c3719dca8f221cd2596c75059ba039478e
SHUVO <im.computer.engineeer@gmail.com>
2025-05-14 23:26:43 +0600

    equal js

---
+++ equal.js
@@ -0,0 +1,20 @@
+// == equal and === equal difference
+// == equal: compares values for equality, allowing type coercion
+// === equal: compares values for equality, without allowing type coercion
+
+
+
+// == equal: its checking the value
+// === equal: its checking the value and the type
+
+
+const a = 5;
+const b = [5];
+
+if (a === b){
+    console.log ('Values are equal');
+}
+
+else {
+    console.log ('Values are not equal');
+}
